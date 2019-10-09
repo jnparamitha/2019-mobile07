54c230bfb18cc774158c589646ace61e6c090421
jnparamitha <jnparamitha@gmail.com>
2019-10-09 14:03:51 +0700

    Splash Screen

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/WelcomeActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package polinema.ac.id.androiduistarter.activities;
+package polinema.ac.id.androiduistarter.fragments.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import polinema.ac.id.androiduistarter.R;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
